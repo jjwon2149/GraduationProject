c33f2a688d6eb9fa6f4085d323a22798ed4e1a51
jjwon2149 <45623603+jjwon2149@users.noreply.github.com>
2023-03-09 17:45:18 +0900

    초기서버 업로드

---
+++ server/12_06/12_06/objectDetection/templates/temp.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import sys
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=2204, threaded=True)
